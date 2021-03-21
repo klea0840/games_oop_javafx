227bd3ff00277ee27dfc2d6a3d96083306eb6fec
Lalina Kairova <klea0840@gmail.com>
2021-03-21 20:17:21 +0400

    2. Сокобан. Тестовое задание. [#488567]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -1,8 +1,40 @@
 package ru.job4j.puzzle;
 
 public class Win {
+
+    public static boolean horizpnt(int[][] board, int row) {
+
+        boolean result = true;
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean vertical(int[][] board, int cell) {
+        boolean result = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][cell] != 1) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        boolean rsl = false;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 1 && (Win.horizpnt(board, i) || Win.vertical(board, j))) {
+                    rsl = true;
+                    break;
+                }
+            }
+        }
         return rsl;
     }
 }
